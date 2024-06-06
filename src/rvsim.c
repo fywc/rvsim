5a6367aaccc245a45b388f2850f7ff3f33decc0a
fywc <imfywc@gmail.com>
2024-06-07 02:05:54 +0800

    finish read elf entry

--- src/rvsim.c
+++ src/rvsim.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+#include "rvsim.h"
+
 int main(int argc, char *argv[]) 
 {
-    printf("hello world!\n");
+    assert(argc > 1);
+
+    machine_t m = {0};
+    machine_load_program(&m, argv[1]);
+
+    printf("entry: 0x%08lx\n", m.mmu.entry);
     return 0;
 }
