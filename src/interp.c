39d84e29400874020364f8d635d753443dc43e3c
fywc <imfywc@gmail.com>
2024-06-07 17:41:30 +0800

    add instruction implementation   1. add 7 insts about load

--- src/interp.c
+++ src/interp.c
@@ -7,12 +7,54 @@ static void func_empty(state_t *state, insn_t *insn) {}
 // lb -> load byte
 static void func_lb(state_t *state, insn_t *insn)
 {
-
+    u64 addr = state->gp_regs[insn->rs1] + (i64)insn->imm;
+    state->gp_regs[insn->rd] = *(i8 *)TO_HOST(addr);
 }
 
+static void func_lh(state_t *state, insn_t *insn)
+{
+    u64 addr = state->gp_regs[insn->rs1] + (i64)insn->imm;
+    state->gp_regs[insn->rd] = *(i16 *)TO_HOST(addr);
+}
+
+static void func_lw(state_t *state, insn_t *insn)
+{
+    u64 addr = state->gp_regs[insn->rs1] + (i64)insn->imm;
+    state->gp_regs[insn->rd] = *(i32 *)TO_HOST(addr);
+}
+
+static void func_ld(state_t *state, insn_t *insn)
+{
+    u64 addr = state->gp_regs[insn->rs1] + (i64)insn->imm;
+    state->gp_regs[insn->rd] = *(i64 *)TO_HOST(addr);
+}
+
+static void func_lbu(state_t *state, insn_t *insn)
+{
+    u64 addr = state->gp_regs[insn->rs1] + (i64)insn->imm;
+    state->gp_regs[insn->rd] = *(u8 *)TO_HOST(addr);
+}
+
+static void func_lhu(state_t *state, insn_t *insn)
+{
+    u64 addr = state->gp_regs[insn->rs1] + (i64)insn->imm;
+    state->gp_regs[insn->rd] = *(u16 *)TO_HOST(addr);
+}
+
+static void func_lwu(state_t *state, insn_t *insn)
+{
+    u64 addr = state->gp_regs[insn->rs1] + (i64)insn->imm;
+    state->gp_regs[insn->rd] = *(u32 *)TO_HOST(addr);
+}
 
 static func_t *funcs[] = {
     func_lb,
+    func_lh,
+    func_lw,
+    func_ld,
+    func_lbu,
+    func_lhu,
+    func_lwu,
     func_empty,
 };
 
