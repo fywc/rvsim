56fccf6390f2c0f7f69aee0b19d36372e1615e81
fywc <imfywc@gmail.com>
2024-06-07 14:20:47 +0800

    add exec framework

--- src/machine.c
+++ src/machine.c
@@ -14,3 +14,19 @@ void machine_load_program(machine_t *m, char *prog)
     m->state.pc = (u64)m->mmu.entry;
     return ;
 }
+
+enum exit_reason_t machine_step(machine_t *m)
+{
+    while (true) {
+        exec_block_interp(&m->state);
+
+        if (m->state.exit_reason == indirect_branch || m->state.exit_reason == direct_branch) {
+            continue;
+        }
+
+        break;
+    }
+    assert(m->state.exit_reason == ecall);
+    return ecall;
+
+}
--- src/rvsim.c
+++ src/rvsim.c
@@ -11,5 +11,11 @@ int main(int argc, char *argv[])
 
     printf("entry: 0x%08lx\n", m.mmu.entry);
     printf("host_alloc: 0x%08lx\n", m.mmu.host_alloc);
+
+    enum exit_reason_t exit_reason;
+    while (true) {
+        exit_reason = machine_step(&m);
+        assert(exit_reason == ecall);
+    }
     return 0;
 }
